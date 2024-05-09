0c05a2dc6f5803c7399b80873f992aa32cc28f1a
Wesley Abdoul <Abdoulwes@gmail.com>
2024-05-09 20:33:15 +0200

    up

--- Frontends/src/components/EditName/EditName.jsx
+++ Frontends/src/components/EditName/EditName.jsx
@@ -1,80 +1,84 @@
- import { useState } from "react";
- import Button from "../Button/Button";
-  import { toggleEditState } from "../../redux/loginSlice";
+import { useState } from "react";
+import Button from "../Button/Button";
 
- //Variable pour manipuler le store redux
- import { useSelector, useDispatch } from "react-redux";
- import { infoUserName } from "../../redux/loginSlice";
- //Importation de la fonction pour le PUT
- import { changeUsername } from "../../services/api.service";
+// Variable pour manipuler le store redux
+import { useSelector, useDispatch } from "react-redux";
+import { infoUserName, toggleEditState } from "../../redux/loginSlice";
 
- const EditName = () => {
+// Importation de la fonction pour le PUT
+import { changeUsername } from "../../services/api.service";
+
+const EditName = () => {
   const dispatch = useDispatch();
 
   const handleCancel = () => {
     dispatch(toggleEditState()); // Déclencher l'action pour annuler l'édition
   };
-    // enregistrement de la slice login depuis le store dans une variable
-    const login = useSelector((state) => state.login);
-    const userProfile = login.userProfile;
-    // Initialisation de la variable avec le store par default et onChange pour récupérer la valeur de l'input
-    const [newUserName, setNewUserName] = useState(userProfile.userName || "");
 
-    const token = login.userToken;
-    const handleChangeUserName = (e) => {
-      setNewUserName(e.target.value);
-    };
-    const handleForm = async (e) => {
-        e.preventDefault();
-        const updateUserName = await changeUsername(newUserName, token)
-        if (updateUserName.status === 200) {
-          dispatch(infoUserName(newUserName));
-          console.log("Le nom d'utilisateur a bien été modifié.", updateUserName.status);
-        } else {
-          console.error("La mise à jour du nom d'utilisateur a échoué.");
-        }
-      };
+  // Enregistrement de la slice login depuis le store dans une variable
+  const login = useSelector((state) => state.login);
+  const userProfile = login.userProfile.body;
+  
+  // Initialisation de la variable avec le store par défaut et onChange pour récupérer la valeur de l'input
+  const [newUserName, setNewUserName] = useState(userProfile.userName || "");
 
-      return (
-        <main className="main bg-dark">
-        <section className="sign-in-content toogle-edit-name">
-          <i className="fa fa-user-circle sign-in-icon"></i>
-          <h1>Edit User info</h1>
-          <form onSubmit={handleForm} onClick={(event) => event.stopPropagation()}>
-            <div className="input-wrapper">
-              <label htmlFor="username">Username</label>
-              <input
-                value={newUserName}
-                onChange={handleChangeUserName}
-                type="text"
-                id="username"
-                placeholder="Tapez votre username"
-              />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor="firstname">First Name</label>
-              <input
-                type="text"
-                id="firstname"
-                disabled
-                value={userProfile.firstName}
-              />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor="lastname">Last Name</label>
-              <input
-                type="text"
-                id="lastname"
-                disabled
-                value={userProfile.lastName}
-              />
-            </div>
-            <Button  btnText={"Save"} className={"sign-in-button"}/>
-          </form>
-            <Button  btnText={"Cancel"} className={"sign-in-button"} onClick={handleCancel}/>
-        </section>
-        </main>
-      );
-    };
-    
-    export default EditName;
+  const token = login.userToken;
+  
+  const handleChangeUserName = (e) => {
+    setNewUserName(e.target.value);
+  };
+
+  const handleForm = async (e) => {
+    e.preventDefault();
+    const updateUserName = await changeUsername(newUserName, token);
+    if (updateUserName.status === 200) {
+      dispatch(infoUserName(newUserName));
+      console.log("Le nom d'utilisateur a bien été modifié.", updateUserName.status);
+    } else {
+      console.error("La mise à jour du nom d'utilisateur a échoué.");
+    }
+  };
+
+  return (
+    <main className="main bg-dark">
+      <section className="sign-in-content toogle-edit-name">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Edit User info</h1>
+        <form onSubmit={handleForm} onClick={(event) => event.stopPropagation()}>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input
+              value={newUserName}
+              onChange={handleChangeUserName}
+              type="text"
+              id="username"
+              placeholder={userProfile.userName}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="firstname">First Name</label>
+            <input
+              type="text"
+              id="firstname"
+              disabled
+              value={userProfile.firstName}
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="lastname">Last Name</label>
+            <input
+              type="text"
+              id="lastname"
+              disabled
+              value={userProfile.lastName}
+            />
+          </div>
+          <Button btnText={"Save"} className={"sign-in-button"}/>
+        </form>
+        <Button btnText={"Cancel"} className={"sign-in-button"} onClick={handleCancel}/>
+      </section>
+    </main>
+  );
+};
+
+export default EditName;
--- Frontends/src/components/User/User.jsx
+++ Frontends/src/components/User/User.jsx
@@ -1,6 +1,6 @@
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-
-//import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Accounts from "../../data/accounts.json";// tableau Json
 import Account from "../Account/Account";//Composant
 import Button from "../Button/Button";
@@ -10,10 +10,21 @@ const User = () => {
   const userProfile = useSelector(state =>(state.login.userProfile))
   const editingName = useSelector(state => state.login.editingName);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [redirected, setRedirected] = useState(false);
   const handleDisplayEdit = () => {
     dispatch(toggleEditState()); // Déclencher l'action pour basculer l'état d'édition
   };
-  
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!userProfile && !token && !redirected)  {
+      // Si aucun token n'est trouvé, rediriger l'utilisateur en rendant une redirection
+      setRedirected(true);
+      navigate("/");
+    }
+  }, [userProfile,redirected,navigate ]);
+  console.log(setRedirected);
+
   const { firstName, lastName } = userProfile && userProfile.body ? userProfile.body : { firstName: '', lastName: '' };
   return (
     <main className="main bg-dark2">
